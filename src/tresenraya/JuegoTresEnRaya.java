791b411544853416adf80765e408790ae6548ec8
Carlos08179 <cdelgadodepaz@gmail.com>
2025-03-10 11:40:34 -0500

    Corrección de mensaje en la interfaz

--- src/tresenraya/JuegoTresEnRaya.java
+++ src/tresenraya/JuegoTresEnRaya.java
@@ -1,9 +1,10 @@
 
 package tresenraya;
 
-
 public class JuegoTresEnRaya {
     public static void main(String[] args) {
+        System.out.println("¡Bienvenido a Tres en Raya con control de versiones en Git!");
+
         Modelo modelo = new ModeloTresEnRaya();
         Vista vista = new VistaConsola();
         Presentador presentador = new PresentadorTresEnRaya(modelo, vista);
